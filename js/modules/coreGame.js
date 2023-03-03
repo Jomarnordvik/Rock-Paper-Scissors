52bad6dbada6089d1f8c7262471ccb739ffe2430
Jomar <112378145+Jomarnordvik@users.noreply.github.com>
2023-03-03 23:45:52 +0100

    wrote core function and variables for the game

---
+++ js/modules/coreGame.js
@@ -0,0 +1,17 @@
+export default function coreGame() {	
+	function game() {
+		let playerScore = 0;
+		let computerScore = 0;
+		let moves = 0;
+	 
+		function playGame() {
+		  const rockBtn = document.querySelector('.rock');
+		  const paperBtn = document.querySelector('.paper');
+		  const scissorBtn = document.querySelector('.scissor');
+		  const playerOptions = [rockBtn, paperBtn, scissorBtn];
+		  const computerOptions = ['rock', 'paper', 'scissors'];
+		}
+	}
+
+	game();
+}
