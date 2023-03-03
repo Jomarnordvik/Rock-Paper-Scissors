a7f893fb3a8a0dd4f769d295dc4c11c37f351247
Jomar <112378145+Jomarnordvik@users.noreply.github.com>
2023-03-03 23:48:22 +0100

    Wrote functions for losing scenarios

--- js/modules/coreGame.js
+++ js/modules/coreGame.js
@@ -71,6 +71,40 @@ export default function coreGame() {
 				}
 			}
 		}
+		
+		function gameOver(playerOptions, movesLeft) {
+			const chooseMove = document.querySelector('.game__move');
+			const result = document.querySelector('.game__result');
+			const reloadBtn = document.querySelector('.game__reload');
+			
+			playerOptions.forEach(function(option) {
+				option.style.display = 'none';
+			});
+			
+			chooseMove.innerText = 'Game Over!!';
+			movesLeft.style.display = 'none';
+			
+			if (playerScore > computerScore) {
+				result.style.fontSize = '2rem';
+				result.innerText = 'You Won The Game';
+				result.style.color = '#308D46';
+			} else if (playerScore < computerScore) {
+				result.style.fontSize = '2rem';
+				result.innerText = 'You Lost The Game';
+				result.style.color = 'red';
+			} else {
+				result.style.fontSize = '2rem';
+				result.innerText = 'Tie';
+				result.style.color = 'grey';
+			}
+			reloadBtn.innerText = 'Restart';
+			reloadBtn.style.display = 'flex';
+			reloadBtn.addEventListener('click', function() {
+				window.location.reload();
+			});
+		}
+		
+		playGame();
 	}
 	
 	game();	 
