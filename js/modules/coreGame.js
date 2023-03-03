08efd81edf2e27315c6596991e2a1e9bc1c93648
Jomar <112378145+Jomarnordvik@users.noreply.github.com>
2023-03-03 23:46:45 +0100

    added handlers for game options

--- js/modules/coreGame.js
+++ js/modules/coreGame.js
@@ -3,15 +3,33 @@ export default function coreGame() {
 		let playerScore = 0;
 		let computerScore = 0;
 		let moves = 0;
-	 
+		
 		function playGame() {
-		  const rockBtn = document.querySelector('.rock');
-		  const paperBtn = document.querySelector('.paper');
-		  const scissorBtn = document.querySelector('.scissor');
-		  const playerOptions = [rockBtn, paperBtn, scissorBtn];
-		  const computerOptions = ['rock', 'paper', 'scissors'];
+			const rockBtn = document.querySelector('.rock');
+			const paperBtn = document.querySelector('.paper');
+			const scissorBtn = document.querySelector('.scissor');
+			const playerOptions = [rockBtn, paperBtn, scissorBtn];
+			const computerOptions = ['rock', 'paper', 'scissors'];
+			
+			function handleOptionClick() {
+				const movesLeft = document.querySelector('.game__movesLeft');
+				moves++;
+				movesLeft.innerText = `Moves Left: ${10 - moves}`;
+				
+				const choiceNumber = Math.floor(Math.random() * 3);
+				const computerChoice = computerOptions[choiceNumber];
+				
+				winner(this.innerText, computerChoice);
+				
+				if (moves == 10) {
+					gameOver(playerOptions, movesLeft);
+				}
+			}
+			
+			playerOptions.forEach(function(option) {
+				option.addEventListener('click', handleOptionClick);
+			});
 		}
 	}
-
-	game();
+	game();	 
 }
